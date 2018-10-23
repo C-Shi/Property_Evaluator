6897b72be07cbd14c063654675cd18106a52b7bd
C-Shi <cshi@ualberta.ca>
2018-10-23 12:56:17 -0600

    seed community population/crime db

--- server/server.js
+++ server/server.js
@@ -8,7 +8,21 @@ const app = require("express")(),
       morgan      = require('morgan'),
       knexLogger  = require('knex-logger');
 
-const PORT = 8080 || process.env.PORT;
+const PORT = 3002 || process.env.PORT;
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("api")
+})
 
 app.listen(PORT, process.env.IP, () => {
   console.log("Server start")
