c8bcd519197799afcee834e710192bb08feb5960
Max <MaxHalleran@gmail.com>
2018-10-23 00:49:07 +0000

    Changed some colors to reflect our chosen palette

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import SearchBox from '../components/SearchBox.jsx'
 import GoogleMap from "../lib/GoogleMap"
 import "../style/Navbar.css"
 
 class Navbar extends Component {
   componentDidMount() {
-    
+
     GoogleMap.autocomplete()
   }
   render() {
