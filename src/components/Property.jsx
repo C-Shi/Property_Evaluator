3ce0130bb5e6e7293b139a82e4374f74afefb979
Max <MaxHalleran@gmail.com>
2018-10-23 23:31:37 +0000

    Cleaned up the property cards

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,43 +1,67 @@
 import React, {Component} from 'react';
 import Chart from './Chart';
-import PropertyCard from './propertyCard';
+import "../style/propertyCard.css";
 
 class Property extends Component {
 
     render() {
-      let crimeInfo = []
+      let crimeTotal = 0;
       for (var key in this.props.location.crime) {
-        crimeInfo.push({type: key, count: this.props.location.crime[key]})
+        crimeTotal += this.props.location.crime[key];
       }
-
-      const crimeInfoElement = crimeInfo.map(each => {
-        return <li>{each.type} - {each.count}</li>
-      })
-
-      var sumCrime =0 ;
-      for (var type of crimeInfo) {
-        sumCrime += type["count"];
-      }
-        console.log("hello", sumCrime);
-        console.log("flood",this.props.location.flood)
-
-      let priceInfo = this.props.location.value.map(each => {
-        return <li>{each.year} - {each.price}</li>
-      })
-
-      let populationInfo = this.props.location.comm_population.map(each => {
-        return <li>{each.year} - {each.population}</li>
-      })
 
     return (
         <div className="card neighbor-info">
           <div className="card-body">
-            <PropertyCard location={this.props.location} />
-            <Chart location={this.props.location}/>
-            <button className="btn btn-danger" onClick={() => {this.props.deleteProperty(this.props.location.address)}}>Delete</button>
+
+              <h5 className="card-header">{this.props.location.address} - {this.props.location.comm_name}</h5>
+
+              <table className="table table-bordered">
+
+                <thead>
+                  <tr>
+                      <th>Year </th>
+                      <th>2017</th>
+                      <th>2016</th>
+                      <th>2015</th>
+                      <th>2014</th>
+                      <th>2013</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <th>Population <button className="btn btn-danger">Show</button></th>
+                      {this.props.location.comm_population.map(each => {
+                      return (
+                        <td>{each.population}</td>
+                      )
+                      })}
+                  </tr>
+
+                  <tr>
+                      <th>Recorded Crime <button className="btn btn-danger">Show</button></th>
+                      <td colSpan="5">{crimeTotal}</td>
+                  </tr>
+                  <tr>
+                    <th>Walkability Score:</th>
+                    <td colSpan="5">Score</td>
+                  </tr>
+                  <tr>
+                    <th>Flood Risk:</th>
+                    <td colSpan="5">{this.props.location.flood ? 'true' : 'false'}</td>
+                  </tr>
+                </tbody>
+              </table>
+
           </div>
         </div>
       );
     }
   }
 export default Property;
+
+// <button className="btn btn-danger">Show</button>
+//
+// <Chart location={this.props.location}/>
+// <button className="btn btn-danger" onClick={() => {this.props.deleteProperty(this.props.location.address)}}>Delete</button>
