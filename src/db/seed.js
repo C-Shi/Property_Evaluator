862e191d93eb528a67051e6333139a1fbb22afa8
C-Shi <cshi@ualberta.ca>
2018-10-22 19:06:12 -0600

    live data on choropleth

---
+++ src/db/seed.js
@@ -0,0 +1,54 @@
+const SeedChoroplethData = {
+  search: {
+    "DOWNTOWN COMMERCIAL CORE": 1,
+    "SAGE HILL": 3, 
+    "CLIFF BUNGALOW": 5, 
+    "CAPITOL HILL": 7, 
+    "ACADIA": 9,
+    "MACEWAN GLEN": 10,
+    "HAMPTONS": 11, 
+    "RUNDLE": 12, 
+    "COUGAR RIDGE": 20, 
+    "HIGHWOOD": 40, 
+    "FOREST LAWN": 50, 
+    "BRIDGELAND/RIVERSIDE": 80,
+    "WOODLANDS": 100,
+    "MAYLAND": 160,
+    "FOOTHILLS": 200, 
+    "TUSCANY": 13,
+    "ROYAL OAK": 9
+  },
+
+  population: {
+    "DOWNTOWN COMMERCIAL CORE": 10,
+    "SAGE HILL": 30, 
+    "CLIFF BUNGALOW": 1, 
+    "CAPITOL HILL": 87, 
+    "ACADIA": 323,
+    "MACEWAN GLEN": 101,
+    "HAMPTONS": 1, 
+    "RUNDLE": 1, 
+    "COUGAR RIDGE": 20, 
+    "HIGHWOOD": 400, 
+    "FOREST LAWN": 50, 
+    "BRIDGELAND/RIVERSIDE": 80,
+    "WOODLANDS": 10,
+    "MAYLAND": 160,
+    "FOOTHILLS": 200, 
+    "TUSCANY": 13,
+    "ROYAL OAK": 9
+  },
+
+  crime: {
+    "DOWNTOWN COMMERCIAL CORE": 12,
+    "SAGE HILL": 3, 
+    "CLIFF BUNGALOW": 53, 
+    "CAPITOL HILL": 73, 
+    "ACADIA": 91,
+    "MACEWAN GLEN": 10,
+    "HAMPTONS": 11, 
+  }
+
+}
+
+export default SeedChoroplethData
