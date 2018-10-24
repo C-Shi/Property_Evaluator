17229f61e015dff30498ac3e8ae3d576a2fb8e49
Max <MaxHalleran@gmail.com>
2018-10-24 22:12:22 +0000

    Restyled the navbar to be closer to mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,35 +11,26 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-default">
         <div className="navbar-links">
-          <a className="navbar-brand"
+          <a className="navbar-link"
              onClick={(e) => {
               e.preventDefault();
               this.props.pageChangeHandler("propertyList")
              }} >
              Real Map
           </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link"
-                   onClick={(e) => {
-                     e.preventDefault()
-                     this.props.pageChangeHandler("choropleth")
-                    }}
-                >Heat Map <span className="sr-only">(current)</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+          <a className="navbar-link"
+             onClick={(e) => {
+               e.preventDefault()
+               this.props.pageChangeHandler("choropleth")
+              }}
+          >Heat Map
+          </a>
         </div>
 
         <form onSubmit={this.props.handleSubmit} className="form-inline my-lg-0 navbar-search">
             <input type="text" id="searchBox" name="address" className="form-control mr-sm-2" placeholder="add another address to compare?"/>
-            <button className="btn btn-outline-secondary my-2 my-sm-0" id="button-addon2"> <i className="fa fa-search" aria-hidden="true"></i> Search</button>
+            <button className="navbar-search-button btn btn-outline-secondary my-2 my-sm-0" id="button-addon2"> <i className="fa fa-search" aria-hidden="true"></i> Search</button>
         </form>
 
       </nav>
