736484531ed4c32d290fb11e62f3e301496ec6e0
C-Shi <cshi@ualberta.ca>
2018-10-24 12:25:33 -0600

    style

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -12,10 +12,10 @@ class SearchBox extends Component {
       return (
         <div>
           <div className="search">
-          <h1 class="display-5 typewriter">Type an address, get REAL data</h1><br></br>
+          <h1 className="display-5 typewriter">Type an address, get REAL data</h1><br></br>
               <form onSubmit={this.props.handleSubmit} className="form-inline">
-                <input type="text" id="searchBox" name="address" className="form-control" placeholder="Enter an address to get its information"/>
-                <button name="search" className="btn btn-outline-secondary search-btn" id="button-addon2"> <i className="fa fa-search" aria-hidden="true"></i> Search</button>
+                <input type="text" id="searchBox" name="address" className="form-control-lg" placeholder="Enter an address to get its information"/>
+                <button name="search" className="btn btn-outline-secondary btn-lg search-btn" id="button-addon2"> <i className="fa fa-search" aria-hidden="true"></i> Search</button>
               </form>
           </div>
         </div>
